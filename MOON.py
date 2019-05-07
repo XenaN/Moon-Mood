f1b3bb2a62211b1aa8be8c24d6bfd7195a8c6a5e
Xena <nikitina.xena@gmail.com>
2019-05-07 19:36:07 +0700

    Added profile for optimisation research

--- MOON.py
+++ MOON.py
@@ -1,48 +1,33 @@
 import sys
 
 from PyQt5.QtWidgets import *
-from fbs_runtime.application_context import ApplicationContext
-# import cProfile, pstats
+import cProfile, pstats
 
 
 from moon_model import MoonModel
 from moon_controller import MoonController
 
 
-# def startMoon():
-#     """
-#     Для работы с программой
-#     """
-#
-#     # pr = cProfile.Profile()
-#     # pr.enable()
-#     app = QApplication(sys.argv)
-#     model = MoonModel()
-#     controller = MoonController(model)
-#
-#     result = app.exec_()
-#
-#     # pr.disable()
-#     # sortby = 'tottime'
-#     # # ps = pstats.Stats(pr).strip_dirs().sort_stats(sortby)
-#     # ps = pstats.Stats(pr).sort_stats(sortby)
-#     # ps.print_stats()
-#
-#     sys.exit(result)
-
 def startMoon():
     """
-    Для создания exe файла
+    Для работы с программой
     """
-    appctxt = ApplicationContext()
 
+    pr = cProfile.Profile()
+    pr.enable()
+    app = QApplication(sys.argv)
     model = MoonModel()
     controller = MoonController(model)
 
-    result = appctxt.app.exec_()
+    result = app.exec_()
+
+    pr.disable()
+    sortby = 'tottime'
+    # ps = pstats.Stats(pr).strip_dirs().sort_stats(sortby)
+    ps = pstats.Stats(pr).sort_stats(sortby)
+    ps.print_stats()
 
     sys.exit(result)
-
 
 if __name__ == '__main__':
     startMoon()
